a58333df5df8c1b941476f77acb3b9f96edc84d5
suphileericka-dotcom <suphileericka@gmail.com>
2026-03-05 13:13:01 +0100

    fix responsive layout and improve register UI

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 const API_URL =
-  import.meta.env.VITE_API_URL ||
-  (typeof window !== "undefined" ? window.location.origin : "");
+  import.meta.env.VITE_API_URL ??
+  "VITE_API_URL=https://ameya-production.up.railway.app"; // ton backend
 
 export const API = `${API_URL}/api`;
