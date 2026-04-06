427a81deaf53adaa1f4eac264cf1dc6fb064583a
suphileericka-dotcom <suphileericka@gmail.com>
2026-04-06 20:59:56 +0200

    Add Capacitor Android and iOS projects

---
+++ frontend/android/app/src/main/java/company/ameya/app/MainActivity.java
@@ -0,0 +1,5 @@
+package company.ameya.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'company.ameya.app',
+  appName: 'Espace Ameya',
+  webDir: 'dist'
+};
+
+export default config;
